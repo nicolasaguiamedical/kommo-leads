979e863a0752beb44e31b7f867fa3138054037d6
nicolasaguiamedical <nicolas.aguiamedical@gmail.com>
2026-04-01 16:03:02 -0300

    lead.js

--- api/lead.js
+++ api/lead.js
@@ -5,17 +5,22 @@ export default async function handler(req, res) {
 
   const { origem, campanha, pagina } = req.body;
 
-  await fetch("https://hook.us2.make.com/SEU_WEBHOOK_AQUI", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      origem,
-      campanha,
-      pagina
-    })
-  });
+  try {
+    await fetch("https://hook.us2.make.com/SEU_WEBHOOK_AQUI", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        origem,
+        campanha,
+        pagina
+      })
+    });
 
-  return res.status(200).json({ success: true });
+    return res.status(200).json({ success: true });
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 }
