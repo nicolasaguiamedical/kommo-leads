5fc81d93acf0e9b409a9b5770074488d394f8a13
nicolasaguiamedical <nicolas.aguiamedical@gmail.com>
2026-04-01 13:19:56 -0300

    lead.js

--- api/lead.js
+++ api/lead.js
@@ -1,1 +1,13 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
 
+  const { origem, campanha, pagina } = req.body;
+
+  return res.status(200).json({
+    origem,
+    campanha,
+    pagina
+  });
+}
